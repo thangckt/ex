6e83dde5c254420ce6a7ce8e1f98ffd78957743b
thangckt <thangckt@gmail.com>
2024-12-13 18:31:22 +0000

    Update from https://github.com/thangckt/main_site/commit/4a92cf5eee8604c10f991f358e266db958f21349

---
+++ _script/py_regular_update_blogpost.py
@@ -0,0 +1,103 @@
+import requests
+
+
+def download_rawtext(url: str, outfile: str = None) -> str:
+    """Download raw text from a URL."""
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(f"Successful download: {url}")
+        text = response.text
+    else:
+        print(f"Failed to download {url}. HTTP Status: {response.status_code}")
+        text = None
+
+    if text and outfile:
+        with open(outfile, "w") as f:
+            f.write(text)
+    return text
+
+
+def create_mkdocsblog_text(
+    text: str,
+    title: str = None,
+    date_post: str = "2024-09-25",
+    label: list[str] = ["Science"],
+):
+    """Create a blog text."""
+
+    ### extract title if not provided
+    if title is None:
+        title_strs = [s for s in text.split("\n") if s.startswith("# ")]
+        if not title_strs:
+            title_strs = [s for s in text.split("\n") if s.startswith("## ")]
+        elif not title_strs:
+            title_strs = [s for s in text.split("\n") if s.startswith("### ")]
+
+        if title_strs:
+            title = title_strs[0].replace("#", "").strip()
+        else:
+            title = "no_title"
+
+    ### make post
+    header = f"---\ntitle: {title}\ndate: {date_post}\nauthors: [thangckt]\ncategories: {label}\ncomments: true\n---\n"
+    blog_text = f"{header}\n {text}"
+
+    ### make filename
+    title_words = title.split()
+    if len(title_words) > 5:
+        title_words = title_words[:5]
+    title_str = "_".join(title_words)
+    filename = f"./_docs/blog/posts/{date_post}_{title_str}.md"
+
+    ### write to file
+    with open(filename, "w") as f:
+        f.write(blog_text)
+    return
+
+
+def update_post(
+    url: str,
+    title: str = None,
+    date_post: str = "2024-09-25",
+    label: list[str] = ["Science"],
+):
+    text = download_rawtext(url)
+    create_mkdocsblog_text(text, title, date_post, label)
+    return
+
+
+##### ANCHOR: udpate the blog posts
+def main():
+    ### Post 1
+    update_post(
+        url="https://raw.githubusercontent.com/JuDFTteam/best-of-atomistic-machine-learning/refs/heads/main/README.md",
+        title="Best of Atomistic Machine Learning",
+        date_post="2024-09-25",
+        label=["Science", "ML", "Python"],
+    )
+    ### Post 2
+    update_post(
+        url="https://raw.githubusercontent.com/ml-tooling/best-of-python-dev/refs/heads/main/README.md",
+        title="Best of Python Developer Tools",
+        date_post="2024-09-28",
+        label=["Python"],
+    )
+    ### Post 3
+    update_post(
+        url="https://raw.githubusercontent.com/ml-tooling/best-of-python/refs/heads/main/README.md",
+        title="Best of Python",
+        date_post="2024-09-30",
+        label=["Python"],
+    )
+    ### Post 4
+    update_post(
+        url="https://raw.githubusercontent.com/vinta/awesome-python/refs/heads/master/README.md",
+        title="Awesome Python",
+        date_post="2024-09-20",
+        label=["Python"],
+    )
+    return
+
+
+if __name__ == "__main__":
+    main()
